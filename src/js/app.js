e8157db84c49860d0699efb511dc39155d00e56a
starikna <don.strekoza@gmail.com>
2023-02-23 23:48:23 +0700

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,58 @@
-export default function healthSort(arrayWarriors = []) {
-  arrayWarriors.sort((prev, next) => next.health - prev.health);
-  return arrayWarriors;
+export default class Character {
+  constructor(name, type, attack, defence, health = 100, level = 1) {
+    this.name = name;
+    this.type = type;
+    this.attack = attack;
+    this.defence = defence;
+    this.health = health;
+    this.level = level;
+
+    this.checkNameLength = function () {
+      if (this.name.length < 2 || this.name.length > 10) {
+        throw new Error('Ошибка длины имени!');
+      }
+    };
+
+    this.checkNameType = function () {
+      if (typeof this.name !== 'string') {
+        throw new Error('Имя - не строка!');
+      }
+    };
+  }
 }
 
-// const arrayOfWarriors = [
-//   {name: 'мечник', health: 10},
-//   {name: 'маг', health: 100},
-//   {name: 'лучник', health: 80},
-// ]
+class Zombie extends Character {
+  constructor(name, type = 'Zombie', attack = 10, defence = 40) {
+    super(name, type, attack, defence);
+  }
+}
 
-// healthSort(arrayOfWarriors);
+class Bowman extends Character {
+  constructor(name, type = 'Bowman', attack = 25, defence = 25) {
+    super(name, type, attack, defence);
+  }
+}
+
+class Swordsman extends Character {
+  constructor(name, type = 'Swordsman', attack = 40, defence = 10) {
+    super(name, type, attack, defence);
+  }
+}
+
+class Magician extends Character {
+  constructor(name, type = 'Magician', attack = 10, defence = 40) {
+    super(name, type, attack, defence);
+  }
+}
+
+class Undead extends Character {
+  constructor(name, type = 'Undead', attack = 25, defence = 25) {
+    super(name, type, attack, defence);
+  }
+}
+
+class Daemon extends Character {
+  constructor(name, type = 'Daemon', attack = 40, defence = 10) {
+    super(name, type, attack, defence);
+  }
+}
