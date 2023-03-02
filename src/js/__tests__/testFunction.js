9c96ebea3e23f872e5d08ff18d4dc92968768f52
starikna <don.strekoza@gmail.com>
2023-03-02 11:33:49 +0700

    4 commit

--- src/js/__tests__/testFunction.js
+++ src/js/__tests__/testFunction.js
@@ -1,31 +1,27 @@
 import { Character } from '../app';
-// import { Zombie } from '../classes/zombie';
+import { Zombie } from '../classes/zombie';
+
+test('Class Zombie is defined', () => {
+  const received = new Zombie('Nat');
+  expect(received).toBeDefined();
+});  
 
 describe('1_test class Character', () => {
   class Zombie extends Character {};
-  // const expected = new Zombie();
   
   test('2_Class is defined', () => {
     expect(Character).toBeDefined();
+  });  
+
+  test('3_test check type === "string"', () => {
+    expect(() => new Zombie('Nat', 321)).toThrow();
   });
 
-  test('3_class Zombie is defined', () => {
-      expect(Zombie).toBeDefined();
+  test('4_test check name length > 11', () => {
+    expect(() => new Zombie('Nattttttttttttttttttt')).toThrow();
   });
 
-  test('4_test check type = "string"', () => {
-    expect(() => new Zombie('Nat', 123)).toThrow();
-  });
-
-  test('5_test check name length', () => {
-    expect(() => new Zombie('N')).toThrow();
-  });
-
-  test('6_test check name = "string"', () => {
+  test('5_test check name = "string"', () => {
     expect(() => new Zombie(123)).toThrow();
   });
-
-  // test('7_test check name = "string"', () => {
-  //   expect(() => {expected.name = 123}).toThrow();
-  // });
-});
+})
